dc08173dee99cfb59b6fc987b3326132bc1cab6d
Vinicius Abismael <vinicius.abismael@gmail.com>
2019-05-10 22:45:57 -0300

    criado o projeto

---
+++ src/main/java/com/herby/pdv_tcc/PdvTccApplication.java
@@ -0,0 +1,13 @@
+package com.herby.pdv_tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herby/pdv_tcc/PdvTccApplicationTests.java
@@ -0,0 +1,16 @@
+package com.herby.pdv_tcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PdvTccApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
