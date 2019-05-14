1e0202b33bf3839a87f59ab9ffb5e4b2850c4dfb
Vinicius Abismael <vinicius.abismael@gmail.com>
2019-05-13 22:48:07 -0300

    Adicionado o banco de dados h2 e implemetacao do dominio

--- src/main/java/com/herby/pdv_tcc/resources/CampanhaResource.java
+++ src/main/java/com/herby/pdv_tcc/resources/CampanhaResource.java
@@ -1,16 +1,29 @@
 package com.herby.pdv_tcc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.herby.pdv_tcc.domain.Campanha;
 
 @RestController
 @RequestMapping(value="/campanhas")
 public class CampanhaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcinando!";
+	public List<Campanha> listar() {
+		
+		Campanha camp1 = new Campanha(1, "Dia das mães");
+		Campanha camp2 = new Campanha(2, "São João");
+		
+		List<Campanha> lista = new ArrayList<>();
+		lista.add(camp1);
+		lista.add(camp2);
+		
+		return lista;
 	}
 
 }
