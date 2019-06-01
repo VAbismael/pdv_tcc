d6243bcebda5329cad3cb7d0352aed3154ee2bca
Vinicius Abismael <vinicius.abismael@gmail.com>
2019-05-31 22:29:01 -0300

    Listando as categorias com o Obj DTO

---
+++ src/main/java/com/herby/pdv_tcc/dto/CategoriaDTO.java
@@ -0,0 +1,40 @@
+package com.herby.pdv_tcc.dto;
+
+import java.io.Serializable;
+
+import com.herby.pdv_tcc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String descricao;
+	
+	public CategoriaDTO() {
+		
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		descricao = obj.getDescricao();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+}
